d25df8bd640dff2092c51a959ac6cadb489b750a
Apprentice <apprentice@dbc-22.local>
2013-10-09 10:30:58 -0700

    update config for rspec and pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeerFlow::Application.config.secret_token = 'ce9ef1053e33b34339a7f4d9ccccb0921283fd3743d25ff787047c884e68a53797931ff115fd5925691cae3db224f372e5456f9ce97bc1695757b7a60b33e2f3'
