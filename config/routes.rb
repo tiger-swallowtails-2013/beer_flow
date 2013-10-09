2d9afa574e1c7e5c4b1c48237113b544f00d105c
Dev Bootcamp <Apprentice@dbc33.local>
2013-10-09 11:44:34 -0700

    discludes unused answer routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 BeerFlow::Application.routes.draw do
 
-  resources :answers
+  resources :answers, :except => [:index, :new, :edit, :update, :destroy]
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
