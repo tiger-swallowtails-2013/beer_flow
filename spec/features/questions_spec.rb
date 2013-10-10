90a2e90ebf14fff51f6f318454b9477f324aecf9
Dev Bootcamp <Apprentice@dbc33.local>
2013-10-09 21:21:00 -0700

    discussion titles on discussions#index link to discussion#show page

--- spec/features/questions_spec.rb
+++ spec/features/questions_spec.rb
@@ -2,12 +2,19 @@ require 'spec_helper'
 
 feature 'submit new question' do
   let(:new_question) { FactoryGirl.build(:question)}
-  it "enters the proper data into the form" do
-    expect{
-      visit new_question_path
-      fill_in 'question[title]', with: new_question.title
-      fill_in 'question[body]', with: new_question.body
-      click_button "Submit"
-    }.to change(Question,:count).by(1)
+  scenario "enters the proper data into the form" do
+    visit new_question_path
+    fill_in 'question[title]', with: new_question.title
+    fill_in 'question[body]', with: new_question.body
+    expect{ click_button "Submit" }.to change(Question,:count).by(1)
+  end
+end
+
+feature 'discussion list' do
+  let!(:new_question) { FactoryGirl.create(:question)}
+  scenario 'discussion links to discussion page' do
+    visit questions_path
+    click_link "#{new_question.title}"
+    expect(page) == question_path(new_question.id)
   end
 end
