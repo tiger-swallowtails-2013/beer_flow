49d73f79434049aa66a9e90e829ba5cac0568194
Tyler <twwaneka@gmail.com>
2013-10-12 21:55:14 -0700

    pushing to master

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,11 +2,12 @@ class AnswersController < ApplicationController
 
   def create
     @answer = Answer.new(params[:answer])
-    if @answer.save
-      render :json => render_to_string(:partial => 'answer', :locals => {:answer => @answer}).to_json
-    else
+    @answer.save
+      #render :json => render_to_string(:partial => 'answer', :locals => {:answer => @answer}).to_json
+
+    # else
       redirect_to question_path(@answer.question)
-    end
+    # end
   end
 
 end
