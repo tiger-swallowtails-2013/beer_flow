99eeacdd7e446f290c2eb3376092ea137353ecb3
Apprentice <apprentice@dbc-22.local>
2013-10-12 20:39:54 -0700

    rids the peeing

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,6 @@
 class VotesController < ApplicationController
 
   def create
-    p params
     vote = Vote.new(user_id: params[:user_id], answer_id: params[:answer_id])
     answer = Answer.find(params[:answer_id])
     vote.save
