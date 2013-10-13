91a8e9266ab261c93c5190dfe81509d0d29ee27e
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-10-12 18:50:09 -0700

    rename routes *sessions *user creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 BeerFlow::Application.routes.draw do
 
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:create, :show]
+  get 'signup', to: 'users#new', as: 'new_user'
+
   resources :questions, only: [:new, :create, :show, :index]
   resources :answers, only: [:create]
 
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:create]
+  get 'login', to: 'sessions#new', as: 'new_session'
+  post 'logout', to: 'sessions#destroy'
 
-  get "logout", to: "sessions#destroy"
   root to: 'questions#index'
 
 end
