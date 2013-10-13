f77b796fd4d3619d6d517078855e313c1770247b
Tyler <twwaneka@gmail.com>
2013-10-12 23:33:39 -0700

    this commit adds the ajax necessary to append the answers to the page as they are created without redirecting'

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -1,11 +1,11 @@
-var Vote = {
+var Answer = {
   init: function() {
     $('form#new_answer').on('ajax:success', this.addQuestion);
-  }
+  },
 
   addQuestion: function(event, data) {
     $('.answers').append(data);
-    $('form#new_answer').get().reset();
+    this.reset();
   }
 
 }
--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,12 +2,11 @@ class AnswersController < ApplicationController
 
   def create
     @answer = Answer.new(params[:answer])
-    @answer.save
-      #render :json => render_to_string(:partial => 'answer', :locals => {:answer => @answer}).to_json
-
-    # else
-      redirect_to question_path(@answer.question)
-    # end
+    if @answer.save
+      render :json => render_to_string(:partial => 'answer', :locals => {:answer => @answer}).to_json
+     else
+      redirect_to root_path
+    end
   end
 
 end
