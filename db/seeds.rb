f96221a0ed14c32266fbc474f7aa3d7fcdc77b68
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-10-13 17:38:37 -0700

    writes seed data file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+USERS = 20
+QUESTIONS = 50
+MAX_ANSWERS = 10
+MAX_VOTES = 10
+
+all_users = []
+USERS.times do
+	all_users << User.create(
+		username: Faker::Internet.user_name,
+		password: 'foobar',
+		password_confirmation: 'foobar'
+		)
+end
+
+QUESTIONS.times do
+	question = Question.create(
+		title: Faker::Lorem.sentence,
+		body: Faker::Lorem.sentence(4)
+	)
+	rand(MAX_ANSWERS).times do
+		answer = Answer.create(
+			body: Faker::Lorem.sentence(2),
+			question_id: question.id
+		)
+		rand(MAX_VOTES).times do
+			Vote.create(
+				user_id: all_users.sample.id,
+				answer_id: answer.id
+			)
+		end
+	end
+end
